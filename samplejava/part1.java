c5359a6deb06cba7249f52933f8a972243953875
jayanthumar <228a1a4444@risekrishnasaiprakasam.edu.in>
2024-10-22 15:46:57 +0530

    Update part1.java

--- samplejava/part1.java
+++ samplejava/part1.java
@@ -1,7 +1,7 @@
 class Pushpa
 {
-public static void main(String args[])
-{
-System.out.println("DEC 6");
-}
+  public static void main(String args[])
+   {
+       System.out.println("DEC 6");
+   }
 }
